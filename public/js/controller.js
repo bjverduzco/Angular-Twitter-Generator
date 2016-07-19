0a250b9c05d6ed7bf03455de3c08fd3aa39be5ee
Ryan Mattson <ryansmattson@gmail.com>
2016-07-19 11:35:22 -0500

    add @ symbolgit statusgit status

--- public/js/controller.js
+++ public/js/controller.js
@@ -40,7 +40,7 @@ angular.module('twitterApp').controller('MainController', function($scope, $http
     $scope.nouns = shuffle($scope.nouns);
     $scope.adjectives = shuffle($scope.adjectives);
     for(var i = 0; i < 10; i++){
-      $scope.twitter_handle[i] = $scope.adjectives[i].adjective + $scope.nouns[i].noun;
+      $scope.twitter_handle[i] = '@' + $scope.adjectives[i].adjective + $scope.nouns[i].noun;
     }
     console.log($scope.twitter_handle);
 
@@ -64,29 +64,6 @@ angular.module('twitterApp').controller('MainController', function($scope, $http
   }
     return array;
 }
-
-// function sortGroups(numGroups) {
-//     var tempArrayOfNames = [];
-//     for (var i = 0; i < arrayOfNames.length; i++) {
-//       tempArrayOfNames[i] = arrayOfNames[i]
-//     }
-//     shuffle(tempArrayOfNames);
-//     groupArray = [];
-//     for (var i = 0; i < numGroups; i++) {
-//       groupArray[i] = [];
-//     }
-//
-//     //while loop checks to be sure after each pass through placing names, there are more names to be placed into groups
-//     while (tempArrayOfNames.length > 0) {
-//       for (var i = 0; i < numGroups; i++) {
-//         //checks to be sure mid while loop that there are still names to be placed
-//         if (tempArrayOfNames.length > 0) {
-//           groupArray[i].push(tempArrayOfNames.shift());
-//         }
-//       }
-//     }
-//     return groupArray;
-//   }
 
 
 });
