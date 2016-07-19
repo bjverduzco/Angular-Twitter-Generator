a92f5c2274de3f15b82854d6997d5817d333598e
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-07-19 10:13:58 -0500

    finished controller.js with the correct get call

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,10 +2,15 @@ angular.module('twitterApp', []);
 
 angular.module('twitterApp').controller('MainController', function($scope, $http){
 
-  $scope.twitterHandle = [];
+  $scope.twitter_handle = [];
+
+  var twitterHandle = function(){
+    $http.get('/twitterHandle').then(handleSuccess, handleFailure);
+  };
 
   function handleSuccess(response){
     console.log('Amazing, it actually worked!!!', response);
+    $scope.twitter_handle = response.data;
   }
 
   function handleFailure(response){
