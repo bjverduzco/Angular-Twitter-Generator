5dc927edd59d0f66f294efd958420b4885274501
bqanderson <bqanderson@me.com>
2016-07-19 09:54:46 -0500

    Add server and routes

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var index = require('./routes/index');
+var pg = require('pg');
+var app = express();
+
+var config = {
+  database: 'twitter_handle',
+  port: 5432
+};
+
+var dbClient = new pg.Client(config);
+
+app.use(express.static('public'));
+
+app.use('/', index);
+
+var server = app.listen(3000, handleServerStart);
+
+function handleServerStart(){
+  var port = server.address().port;
+  console.log('Listening on port: ', port);
+}
