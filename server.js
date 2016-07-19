dc32191496588b0a4ef543a78bf4ef80b8e96712
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-07-19 10:52:01 -0500

    get /adjectives added

--- server.js
+++ server.js
@@ -20,7 +20,37 @@ app.get('/nouns', function(request, response){
     if(err){
       console.log('Connection error', err);
     }
-    client.query('SELECT * FROM nouns;', function(err, result){
+    client.query('SELECT noun FROM nouns;', function(err, result){
+      var nounList = {};
+      console.log(result.rows);
+      nounList = result.rows;
+      if(err){
+        console.log('Query error', err);
+        response.sendStatus(500);
+      } else {
+        console.log('Great success', nounList);
+        response.send(nounList);
+        response.sendStatus(200);
+      }
+
+      client.end(function(err){
+        if(err){
+          console.log('Disconnect error', err);
+        }
+      })
+    })
+  })
+
+})
+
+app.get('/adjectives', function(request, response){
+  var client = new pg.Client(config);
+
+  client.connect(function(err){
+    if(err){
+      console.log('Connection error', err);
+    }
+    client.query('SELECT adjective FROM adjectives;', function(err, result){
       var nounList = {};
       console.log(result.rows);
       nounList = result.rows;
